6588a0b594a4e9cfb00576fbfd95257f89365dec
COR Coding <corcoding@CORs-MacBook-Air.local>
2023-10-26 14:46:39 -0400

    updated post route

--- front-end/src/components/Registration.jsx
+++ front-end/src/components/Registration.jsx
@@ -1,44 +1,87 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react';
+
 const Registration = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+    reEnterPassword: '',
+    secquestion: '',
+    secanswer: '',
+    email: '',
+  });
 
-  const [errorMessage, setErrorMessage] = useState(null)
-  const [formData, setformData] = 
-    useState({
-      username:"",
-      password:"",
-      reEnterPassword:"",
-      securityQuestion:"",
-      securityAnswer:"",
-      email:""
-})
-const handleSubmit = async (e) => {
-  e.preventDefault();
-  console.log(formData);
-};
-const handleChange = (e) => {
-  const { name, value } = e.target;
-  setformData({ ...formData, [name]: value });
-};
+  const [errorFound, setErrorFound] = useState('');
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
+    // Check if the passwords match
+    if (formData.password !== formData.reEnterPassword) {
+      setErrorFound('Passwords do not match');
+      return; // Stop form submission
+    } else {
+      setErrorFound(''); // Reset error message
+    }
 
+    // Proceed with form submission
+    console.log(formData);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
   return (
     <div>
+      <div>{errorFound && <div className="error">{errorFound}</div>}</div>
       Registration
-      <div>{errorMessage}</div>
       <form action="/Registration" method="post" onSubmit={handleSubmit}>
-      <input type="text" name='username' placeholder='Username' required onChange={handleChange}/>
-      <input type="password" name='password' placeholder='Password' required onChange={handleChange}/>
-      <input type="password" name='reEnterPassword' placeholder='Confirm Password' required onChange={handleChange}/>
-      <input type="text" name='securityQuestion' placeholder='Security Question' required onChange={handleChange}/>
-      <input type="text" name='securityAnswer' placeholder='Security Answer' required onChange={handleChange}/>
-      <input type="email" name='email' placeholder='Email' required onChange={handleChange}/>
-      {/* <input type="submit" /> */}
-      <button type='submit'>Submit</button>
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="reEnterPassword"
+          placeholder="Confirm Password"
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="secquestion"
+          placeholder="Security Question"
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="secanswer"
+          placeholder="Security Answer"
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          required
+          onChange={handleChange}
+        />
+        <button type="submit">Submit</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Registration
+export default Registration;
