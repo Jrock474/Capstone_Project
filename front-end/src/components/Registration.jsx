89ffdf8ec3155dbc9ccd73d1c3d5e151a9e0cc14
COR Coding <corcoding@CORs-MacBook-Air.local>
2023-10-26 10:52:24 -0400

    set up basic registration logic

--- front-end/src/components/Registration.jsx
+++ front-end/src/components/Registration.jsx
@@ -1,16 +1,39 @@
 import React from 'react'
-
+import { useState } from 'react'
 const Registration = () => {
+const [formData, setformData] = 
+useState({
+  username:"",
+  password:"",
+  reEnterPassword:"",
+  securityQuestion:"",
+  securityAnswer:"",
+  email:""
+})
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  console.log(formData);
+};
+const handleChange = (e) => {
+  const { name, value } = e.target;
+  setformData({ ...formData, [name]: value });
+};
+
+
+
   return (
     <div>
       Registration
-      <input type="text" name='username' placeholder='Username' required/>
-      <input type="password" name='password' placeholder='Password' required/>
-      <input type="password" name='reEntePassword' placeholder='Confirm Password' required/>
-      <input type="text" name='securityQuestion' placeholder='Security Question' required/>
-      <input type="text" name='securityAnswer' placeholder='Security Answer' required/>
-      <input type="email" name='email' placeholder='Email' required/>
-      <input type="submit" />
+      <form onSubmit={handleSubmit}>
+      <input type="text" name='username' placeholder='Username' required onChange={handleChange}/>
+      <input type="password" name='password' placeholder='Password' required onChange={handleChange}/>
+      <input type="password" name='reEnterPassword' placeholder='Confirm Password' required onChange={handleChange}/>
+      <input type="text" name='securityQuestion' placeholder='Security Question' required onChange={handleChange}/>
+      <input type="text" name='securityAnswer' placeholder='Security Answer' required onChange={handleChange}/>
+      <input type="email" name='email' placeholder='Email' required onChange={handleChange}/>
+      {/* <input type="submit" /> */}
+      <button type='submit'>Submit</button>
+      </form>
     </div>
   )
 }
