3495ed9cdd27382ae6073d9560658005783db7d1
COR Coding <corcoding@CORs-MacBook-Air.local>
2023-10-26 11:23:24 -0400

    updated registration

--- back-end/server.js
+++ back-end/server.js
@@ -1,19 +1,32 @@
 const express = require("express")
-
-
+const bcrypt = require("bcrypt")
+const pg = require("pg-promise")()
 const app = express()
 const port = 3000
 
+const db =pg("postgres://oibmtylz:pr7Db6Isdh55dkfrm-md5ItPFhA1IPQi@suleiman.db.elephantsql.com/oibmtylz")
 // const pg = require('pg-promise')();
 
-// const db = pg("postgres://dretaylor@localhost:5432/postgres");
+// url - postgres://oibmtylz:pr7Db6Isdh55dkfrm-md5ItPFhA1IPQi@suleiman.db.elephantsql.com/oibmtylz
 
 app.get('/', (req, res) => {
     res.send("ayo")
 })
 app.post('/Registration', async (req, res) => {
-    const { username, email, password,reEnterPassword, securityQuestion, securityAnswer } = req.body;
-    res.send("ayo")
+    const { username, email, password, reEnterPassword, securityQuestion, securityAnswer } = req.body;
+    res.send("registered")
+     // Generate a salt and hash the password
+     const saltRounds = 10; // You can adjust the number of salt rounds for more security
+     const hashedPassword = await bcrypt.hash(password, saltRounds);
+  // Create a new user with the hashed password
+  const newUser = await users.create({
+    username,
+    email,
+    securityQuestion,
+    securityAnswer,
+    password: hashedPassword, 
+    reEnterPassword: hashedPassword// Store the hashed password in the database
+  });
 })
 
 app.listen(port, ()=>{
