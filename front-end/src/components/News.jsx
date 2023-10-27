9f9e08a35a5b6f8c4eb7e2b0a4d8c14082cc1c43
COR Coding <corcoding@CORs-MacBook-Air.local>
2023-10-27 13:22:50 -0400

    added News API data

--- front-end/src/components/News.jsx
+++ front-end/src/components/News.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const News = () => {
-  return (
-    <div>News</div>
-  )
-}
+  const [newsData, setNewsData] = useState([]);
+  const apiKey = "1f03aff0ade84d56aff9e17c77b3f878";
 
-export default News
+  const fetchNews = async () => {
+    const response = await fetch(`https://newsapi.org/v2/everything?q=gaming&apiKey=${apiKey}`);
+    const data = await response.json();
+    setNewsData(data.articles);
+  };
+
+  return (
+    <div>
+      <button onClick={fetchNews}>Fetch News</button>
+      <ul>
+        {newsData.slice(0,3).map((article, index) => (
+          <li key={index}>
+            <strong>{article.title}</strong>
+            <p>{article.description}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default News;
