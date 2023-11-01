805489ec5f4e63d6d60cfeced0b2dc5ddc695c78
Tony <sonnblom03@gmail.com>
2023-11-01 15:07:13 -0400

    styling

--- front-end/src/components/MainGame.jsx
+++ front-end/src/components/MainGame.jsx
@@ -5,22 +5,22 @@ const MainGame = () => {
     <div>
       <div className="GameBody">
         <div className="Pet" id="Pet">
-          <img src={"/gifs/DinoStill.gif"} style= {{ height: 500 }} /> 
+          <img src={"/gifs/DinoStill.gif"} style= {{ height: 350 }} /> 
         </div>
         <div className="MoodBox">
-          <img src={"/images/MoodAngry.png"} style= {{ height: 150 }} />
-            <img src={"/images/MoodDiscomfort.png"} style= {{ height: 150 }} />
-              <img src={"/images/MoodSick.png"} style= {{ height: 150 }} />
+          <img src={"/images/MoodAngry.png"} style= {{ height: 100 }} />
+            <img src={"/images/MoodDiscomfort.png"} style= {{ height: 100 }} />
+              <img src={"/images/MoodSick.png"} style= {{ height: 100 }} />
               </div>
               <div className="ActivityBox">
-                <img src={"/images/FightIcon.png"} style= {{ height: 150 }} />
-                  <img src={"/images/FoodIcon.png"} style= {{ height: 150 }} />
-                    <img src={"/images/MedicineIcon.png"} style= {{ height: 150 }} />
-                      <img src={"/images/BathIcon.png"} style= {{ height: 150 }} />
-                        <img src={"/images/PlayIcon.png"} style= {{ height: 150 }} />
+                <img src={"/images/FightIcon.png"} style= {{ height: 100 }} />
+                  <img src={"/images/FoodIcon.png"} style= {{ height: 100 }} />
+                    <img src={"/images/MedicineIcon.png"} style= {{ height: 100 }} />
+                      <img src={"/images/BathIcon.png"} style= {{ height: 100 }} />
+                        <img src={"/images/PlayIcon.png"} style= {{ height: 100 }} />
                         </div>
 
-                        <img src={"/images/Poop.png"} id="Poop" style= {{ height: 150 }} />
+                        <img src={"/images/Poop.png"} id="Poop" style= {{ height: 100 }} />
 
                         </div>
                       </div>
