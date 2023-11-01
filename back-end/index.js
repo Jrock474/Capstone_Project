20803b1710d5eaf08e142a79473945153b069e2a
COR Coding <corcoding@CORs-MacBook-Air.local>
2023-11-01 15:16:07 -0400

    changed images in caruosel

--- back-end/index.js
+++ back-end/index.js
@@ -285,7 +285,25 @@ app.delete('/Delete', async (req, res) => {
     return res.status(500).send('User deletion failed');
   }
 });
+//placeholder logic from the documentation
+app.get('/logout', function (req, res, next) {
+  // logout logic
 
+  // clear the user from the session object and save.
+  // this will ensure that re-using the old session id
+  // does not have a logged in user
+  req.session.user = null
+  req.session.save(function (err) {
+    if (err) next(err)
+
+    // regenerate the session, which is good practice to help
+    // guard against forms of session fixation
+    req.session.regenerate(function (err) {
+      if (err) next(err)
+      res.redirect('/')
+    })
+  })
+})
 
 
 // app.post('/sendEmail', async (req, res) => {
