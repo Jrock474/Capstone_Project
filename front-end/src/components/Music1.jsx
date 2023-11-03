03ccb45aa2cbffffc132573ebb17985a11d4eba5
COR Coding <corcoding@CORs-Air.localdomain>
2023-11-03 09:57:35 -0400

    removed news component, added music component

---
+++ front-end/src/components/Music1.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+function Music1() {
+  return (
+    <div>
+      <audio autoPlay >
+        <source src="/Kaizo.mp3" type="audio/mp3" />
+      </audio>
+    </div>
+  );
+}
+export default Music1;
