de1bc8c4812c7275ce84fac4ac17d6c6c9cc4fbe
Ethan <you@example.com>
2023-11-07 14:48:31 -0500

    adds information on the readMe

--- front-end/src/components/MainGame.jsx
+++ front-end/src/components/MainGame.jsx
@@ -196,7 +196,7 @@ const MainGame = () => {
                         {/* <Timer /> */}
                         <img src={isPoopActive ? "/images/Poop.png" : null} id="Poop" style= {{ height: 100 }} />
                         </div>
-                        <Music1/>                      
+                        <Music1/>                 
                       </div>
                       )
 }
